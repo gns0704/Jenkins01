37199ab9481fe14d76177910ecc0e39712977ffc
gns0704 <gynam@hanmail.net>
2025-08-13 17:07:48 +0900

    four

--- src/main/java/com/example/demo/MyHome.java
+++ src/main/java/com/example/demo/MyHome.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class MyHome {
 
     @GetMapping("/")
     public String home(){
-        return "home";
+        return "aaaaaaaaaaaaaaaaaaa";
     }
 }
